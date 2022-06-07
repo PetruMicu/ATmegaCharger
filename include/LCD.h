d4f29f759f5a85ed6d12fbe891f3c6ee83836898
micup <micu.petru2899@gmail.com>
2022-06-07 08:36:51 +0300

    deleted LCD files (not needed anymore)

--- include/LCD.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Andreea on 5/18/2022.
-//
-
-#ifndef ATMEGA164A_LCD_H
-#define ATMEGA164A_LCD_H
-
-#endif //ATMEGA164A_LCD_H
--- src/LCD.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Andreea on 5/18/2022.
-//
-
-#include "../include/LCD.h"
